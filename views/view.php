ef723cc4ce9b0a6d915a9a126bb617fd76de21df
Critick <andrew.stezenko@gmail.com>
2019-11-15 23:16:05 +0300

    Outputing list of terms from 'products' taxonomy.

--- views/view.php
+++ views/view.php
@@ -2,4 +2,29 @@
 if ( !defined( 'FW' ) ) {
 	die( 'Forbidden' );
 }
+
+// Get terms by taxonomy slug.
+$terms = get_terms(
+	[
+		'taxonomy'		=> 'products',	// Taxonomy slug.
+		'hide_empty'	=> true 	// Do not show empty terms.
+	]
+);
 ?>
+
+<!-- List of terms from 'products' taxonomy. -->
+<ul class = "terms">
+	<?php
+	// Each term name from 'products' taxonomy outputing in loop.
+	foreach ( $terms as $key => $term ) {
+		if ( $key === 0 ) {
+			echo '<li class = "term term_active">';
+		}	else {
+			echo '<li class = "term">';
+		}
+
+		esc_html_e( $term->name );
+		echo '</li>';
+	}
+	?>
+</ul>
