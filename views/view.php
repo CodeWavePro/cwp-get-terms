5e817564c788cfc6c8e86e10c04fe83e20ceb5c5
Critick <andrew.stezenko@gmail.com>
2019-12-27 23:30:18 +0300

    Removed currency from old price in more info popup.

--- views/view.php
+++ views/view.php
@@ -327,9 +327,6 @@ $preloader_icon = ( isset( $atts['preloader_icon'] ) && $atts['preloader_icon'] 
 
 				<span class = "cwpgt-more-info-prices__old">
 					<span class = "cwp-more-info-prices__value"></span>
-					<span class = "cwp-more-info-prices__currency">
-						<?php echo $currency_icon ?>
-					</span>
 				</span>
 				<span class = "cwpgt-more-info-prices__new">
 					<span class = "cwp-more-info-prices__value"></span>
